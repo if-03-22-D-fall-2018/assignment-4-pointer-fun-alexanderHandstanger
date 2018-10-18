60dee5ebd1aef75700feae8c3d782914cab34afe
Alexander Handstanger <zombeyfan.ah@gmail.com>
2018-10-18 08:21:21 +0200

    Start

---
+++ PointerFun.c
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / 2DHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 4
+ * Title:			Pointer_Fun
+ * Author:			Alexander Handstanger
+ * ----------------------------------------------------------
+ * Description:
+ * This assignment lets you practise a bit more with the
+ * difference between references and values.
+ * ----------------------------------------------------------
+ */
+
+
+#include <studio.h>
+#include <float.h>
+#include <string.h>
+
+int main(int argc, char const *argv[]) {
+  
+}
